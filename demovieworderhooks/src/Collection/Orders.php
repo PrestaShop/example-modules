6cb9b850f0be79a46840724378e09f7090b333d6
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-02-27 14:33:53 +0200

    changed licenses to AFL

--- demovieworderhooks/src/Collection/Orders.php
+++ demovieworderhooks/src/Collection/Orders.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * 2007-2020 Friends of PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
 
 declare(strict_types=1);
 
