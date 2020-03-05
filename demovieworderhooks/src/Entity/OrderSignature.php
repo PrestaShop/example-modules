7df3f1790bea2ebf208c1e96b6288f004206468e
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-03-05 11:08:43 +0200

    changing db table signature to order_signature and related entity Signature

---
+++ demovieworderhooks/src/Entity/OrderSignature.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\DemoViewOrderHooks\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="PrestaShop\Module\DemoViewOrderHooks\Repository\SignatureRepository")
+ */
+class OrderSignature
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id_signature", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_order", type="integer")
+     */
+    private $orderId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="filename", type="string")
+     */
+    private $filename;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return OrderSignature
+     */
+    public function setId(int $id): OrderSignature
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string $filename
+     *
+     * @return OrderSignature
+     */
+    public function setFilename(string $filename): OrderSignature
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrderId(): int
+    {
+        return $this->orderId;
+    }
+
+    /**
+     * @param int $orderId
+     *
+     * @return OrderSignature
+     */
+    public function setOrderId(int $orderId): OrderSignature
+    {
+        $this->orderId = $orderId;
+
+        return $this;
+    }
+}
