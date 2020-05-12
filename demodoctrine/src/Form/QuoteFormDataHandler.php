d083de2e11608f4807c237e4caebbc40850f1f07
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-05-12 14:58:28 +0200

    Update License header

--- demodoctrine/src/Form/QuoteFormDataHandler.php
+++ demodoctrine/src/Form/QuoteFormDataHandler.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * 2007-2020 PrestaShop
+ * 2007-2020 PrestaShop SA and Contributors
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
  * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
  */
+declare(strict_types=1);
 
 namespace PrestaShop\Module\DemoDoctrine\Form;
 
