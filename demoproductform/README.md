ce90fe891854613bf543e4a204836ba62b2807d1
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-04-26 14:41:51 +0300

    init empty module skeleton

---
+++ demoproductform/README.md
@@ -0,0 +1,18 @@
+# Module demoproductform
+
+## About
+
+This is example module explaining various extendability options in product page form (from v1.7.8)
+
+### Supported PrestaShop versions
+
+Compatible with and 1.7.8.0 and above versions.
+
+### Requirements
+
+1. Composer, see [Composer](https://getcomposer.org/) to learn more
+
+#### How to install:
+1. Copy the module into `modules` directory of your PrestaShop installation
+2. `cd` into the module's directory and run `composer install` to download dependencies into vendor directory
+3. Install the module from Back Office
---
+++ demoproductform/demoproductform.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+
+declare(strict_types=1);
+
+use PrestaShop\Module\DemoProductForm\Install\Installer;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__.'/vendor/autoload.php';
+
+class DemoProductForm extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'demoproductform';
+        $this->author = 'PrestaShop';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = ['min' => '1.7.7.0', 'max' => _PS_VERSION_];
+
+        parent::__construct();
+
+        $this->displayName = $this->l('DemoProductForm');
+        $this->description = $this->l('DemoProductForm module description');
+    }
+
+    /**
+     * @return bool
+     */
+    public function install()
+    {
+        if (!parent::install()) {
+            return false;
+        }
+
+        $installer = new Installer();
+
+        return $installer->install($this);
+    }
+}
