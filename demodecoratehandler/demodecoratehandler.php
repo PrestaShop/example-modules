70f3e3da3626b1585c5eef3fdc68c83e9e70c773
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-05-19 10:58:46 +0300

    init empty module

---
+++ demodecoratehandler/demodecoratehandler.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+
+declare(strict_types=1);
+
+use PrestaShop\Module\DemoDecorateHandler\Install\Installer;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__.'/vendor/autoload.php';
+
+class DemoDecorateHandler extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'skeleton';
+        $this->author = 'PrestaShop';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = ['min' => '1.7.7.0', 'max' => _PS_VERSION_];
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Skeleton');
+        $this->description = $this->l('Skeleton module description');
+    }
+
+    /**
+     * @return bool
+     */
+    public function install()
+    {
+        if (!parent::install()) {
+            return false;
+        }
+
+        $installer = new Installer();
+
+        return $installer->install($this);
+    }
+}
---
+++ demodecoratehandler/src/Install/Installer.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\DemoDecorateHandler\Install;
+
+use Db;
+use Module;
+
+class Installer
+{
+    /**
+     * Module's installation entry point.
+     *
+     * @param Module $module
+     *
+     * @return bool
+     */
+    public function install(Module $module): bool
+    {
+        if (!$this->registerHooks($module)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Register hooks for the module.
+     *
+     * @see https://devdocs.prestashop.com/1.7/modules/concepts/hooks/
+     *
+     * @param Module $module
+     *
+     * @return bool
+     */
+    private function registerHooks(Module $module): bool
+    {
+        $hooks = [
+            //@todo: register hooks used in module e.g.:
+            'displayHeader',
+            'actionDispatcherBefore'
+        ];
+
+        return (bool) $module->registerHook($hooks);
+    }
+
+    /**
+     * A helper that executes multiple database queries.
+     *
+     * @param array $queries
+     *
+     * @return bool
+     */
+    private function executeQueries(array $queries): bool
+    {
+        foreach ($queries as $query) {
+            if (!Db::getInstance()->execute($query)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
