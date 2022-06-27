66aa7c44df2ffb2c409688c50ca68415ead16c11
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-06-27 19:54:53 +0200

    Adapt modifier to recent product form changes

--- demoproductform/src/Form/Modifier/ProductFormModifier.php
+++ demoproductform/src/Form/Modifier/ProductFormModifier.php
@@ -16,6 +16,7 @@ use PrestaShop\Module\DemoProductForm\CQRS\CommandHandler\SaveMyModuleCustomFiel
 use PrestaShop\Module\DemoProductForm\Entity\CustomProduct;
 use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
 use PrestaShopBundle\Form\Admin\Type\SwitchType;
+use PrestaShopBundle\Form\FormBuilderModifier;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -55,8 +56,7 @@ final class ProductFormModifier
         FormBuilderInterface $productFormBuilder,
         array $formData
     ): void {
-        $this->modifyBasicTab($productId, $productFormBuilder);
-        $this->modifyShortcutTab($productFormBuilder, $formData);
+        $this->modifyDescriptionTab($productId, $productFormBuilder);
         $this->modifyFooter($productFormBuilder);
     }
 
@@ -66,14 +66,14 @@ final class ProductFormModifier
      *
      * @see SaveMyModuleCustomFieldHandler to check how the field is handled on form POST
      */
-    private function modifyBasicTab(?ProductId $productId, FormBuilderInterface $productFormBuilder): void
+    private function modifyDescriptionTab(?ProductId $productId, FormBuilderInterface $productFormBuilder): void
     {
         $idValue = $productId ? $productId->getValue() : null;
         $customProduct = new CustomProduct($idValue);
 
-        $basicTabFormBuilder = $productFormBuilder->get('basic');
+        $descriptionTabFormBuilder = $productFormBuilder->get('description');
         $this->formBuilderModifier->addAfter(
-            $basicTabFormBuilder,
+            $descriptionTabFormBuilder,
             'description',
             'demo_module_custom_field',
             TextType::class,
@@ -91,24 +91,7 @@ final class ProductFormModifier
                 // this is just an example, but in real case scenario you could have some data provider class to wrap more complex cases
                 'data' => $customProduct->custom_field,
                 'empty_data' => '',
-            ]
-        );
-    }
-
-    /**
-     * @param FormBuilderInterface $productFormBuilder
-     * @param array $formData
-     */
-    private function modifyShortcutTab(FormBuilderInterface $productFormBuilder, array $formData): void
-    {
-        // This is just an example of adding a field in the shortcuts column (the data is not saved)
-        $shortcutsFormBuilder = $productFormBuilder->get('shortcuts');
-        $retailPriceFormBuilder = $shortcutsFormBuilder->get('retail_price');
-        $this->formBuilderModifier->addBefore($retailPriceFormBuilder, 'price_tax_included',
-            'additional_shipping_cost',
-            MoneyType::class,
-            [
-                'data' => $formData['shipping']['additional_shipping_cost'] ?? 0.0,
+                'form_theme' => '@PrestaShop/Admin/TwigTemplateForm/prestashop_ui_kit_base.html.twig',
             ]
         );
     }
@@ -118,9 +101,13 @@ final class ProductFormModifier
      */
     private function modifyFooter(FormBuilderInterface $productFormBuilder): void
     {
-        $headerFormBuilder = $productFormBuilder->get('header');
+        $headerFormBuilder = $productFormBuilder->get('footer');
         $headerFormBuilder->add('forms_info', SwitchType::class, [
-            'label' => $this->translator->trans('Form infos', [], 'Modules.Demoproductform.Admin'),
+            'label' => false,
+            'choices' => [
+                $this->translator->trans('Hide form infos', [], 'Modules.Demoproductform.Admin') => false,
+                $this->translator->trans('Show form infos', [], 'Modules.Demoproductform.Admin') => true,
+            ],
             'label_help_box' => $this->translator->trans(
                 'You can display information about each form part to see where it is included in the Product form structure, the information is shown when mouse is over the element.',
                 [],
