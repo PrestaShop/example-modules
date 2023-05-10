395bddefb252689536def2caa98b846565506fa8
AUTHIE Louis <contact@louisauthie.fr>
2023-05-10 22:44:18 +0200

    test geocoordinatesFormType

---
+++ demosymfonyform/src/Controller/DemoGeoCoordinatesFormController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\DemoSymfonyForm\Controller;
+
+use PrestaShop\Module\DemoSymfonyForm\Form\GeoCoordinatesType;
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DemoGeoCoordinatesFormController extends FrameworkBundleAdminController
+{
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(GeoCoordinatesType::class, []);
+
+        return $this->render('@Modules/demosymfonyform/views/templates/admin/formGeoCoordinates.html.twig', [
+            'demoGeoCoordinatesForm' => $form->createView(),
+        ]);
+    }
+}
