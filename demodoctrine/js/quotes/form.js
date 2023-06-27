a6e488201239e534c11fd212b5bbd2643218c1b1
Krystian Podemski <podemski.krystian@gmail.com>
2023-06-27 15:04:44 +0200

    fix

--- demodoctrine/js/quotes/form.js
+++ demodoctrine/js/quotes/form.js
@@ -7,7 +7,7 @@
  * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
  */
 
-// Since PrestaShop 1.7.8 you can import belows component directly from the window object.
+// Since PrestaShop 1.7.8 you can import components below directly from the window object.
 // This is the recommended way to use them as you won't have to do any extra configuration or compilation.
 // We keep the old way of importing them for backward compatibility.
 // @see: https://devdocs.prestashop-project.org/1.7/development/components/global-components/
