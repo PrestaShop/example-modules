43edfb76b249542c3f588def28b6b6c0acfa2ef3
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2025-03-28 18:55:46 +0100

    Adapt module democonsolecommand for V9

--- democonsolecommand/democonsolecommand.php
+++ democonsolecommand/democonsolecommand.php
@@ -1,12 +1,28 @@
 <?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
 
 declare(strict_types=1);
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
-
-require_once __DIR__.'/vendor/autoload.php';
 
 class DemoConsoleCommand extends Module
 {
@@ -15,11 +31,11 @@ class DemoConsoleCommand extends Module
         $this->name = 'democonsolecommand';
         $this->author = 'PrestaShop';
         $this->version = '1.0.0';
-        $this->ps_versions_compliancy = ['min' => '1.7.7.0', 'max' => '8.99.99'];
+        $this->ps_versions_compliancy = ['min' => '9.0.0', 'max' => _PS_VERSION_];
 
         parent::__construct();
 
-        $this->displayName = $this->l('Demo - implement Symfony command for console');
-        $this->description = $this->l('Shows example how to implement Symfony command for console');
+        $this->displayName = $this->trans('Demo - implement Symfony command for console', [], 'Modules.DemoConsoleCommand.Admin');
+        $this->description = $this->trans('Shows example how to implement Symfony command for console', [], 'Modules.DemoConsoleCommand.Admin');
     }
 }
