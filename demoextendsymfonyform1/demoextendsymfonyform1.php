77eb5fe0958559faf418eca744e396b7cf6e2783
Krystian Podemski <podemski.krystian@gmail.com>
2026-03-12 11:17:37 +0100

    Update URLs to docs

--- demoextendsymfonyform1/demoextendsymfonyform1.php
+++ demoextendsymfonyform1/demoextendsymfonyform1.php
@@ -79,7 +79,7 @@ class DemoExtendSymfonyForm1 extends Module
     /**
      * Install module and register hooks to allow grid modification.
      *
-     * @see https://devdocs.prestashop.com/1.7/modules/concepts/hooks/use-hooks-on-modern-pages/
+     * @see https://devdocs.prestashop.com/9/modules/concepts/hooks/use-hooks-on-modern-pages/
      *
      * @return bool
      */
